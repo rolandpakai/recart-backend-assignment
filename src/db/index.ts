618ed1a7043f2b8cf7b816d8629231d5ba0894c2
roland.pakai <roland.pakai@lightware.com>
2025-03-01 13:29:25 +0100

    feat: created index for db functions

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import connectToMongoDbWithRetries from "./connectToMongoDbWithRetries";
+import connectToMongoDb from './connectToMongoDb';
+import seedDb from './seedDb';
+
+export { 
+  connectToMongoDbWithRetries,
+  connectToMongoDb,
+  seedDb,
+}; 
